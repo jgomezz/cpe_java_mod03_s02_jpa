97ac70ca02e0dd70d2b3d459f239ab905a0f0856
jgomezz <jgomezz@gmail.com>
2026-05-03 11:30:43 -0500

    update

--- src/main/java/pe/edu/tecsup/app/entities/Categoria.java
+++ src/main/java/pe/edu/tecsup/app/entities/Categoria.java
@@ -1,15 +1,29 @@
 package pe.edu.tecsup.app.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//@AllArgsConstructor
-//@NoArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
+@Entity
+@Table(name="categorias")
 public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre")
     private String nombre;
+
+    @Column(name = "orden")
     private Integer orden;
+
+
 
 }
--- src/main/java/pe/edu/tecsup/app/repositories/CategoriaRepository.java
+++ src/main/java/pe/edu/tecsup/app/repositories/CategoriaRepository.java
@@ -1,10 +1,12 @@
 package pe.edu.tecsup.app.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.tecsup.app.entities.Categoria;
 
 import java.util.List;
 
-public interface CategoriaRepository {
+public interface CategoriaRepository
+        extends JpaRepository<Categoria, Long> {
 
     List<Categoria> findAll();
 
