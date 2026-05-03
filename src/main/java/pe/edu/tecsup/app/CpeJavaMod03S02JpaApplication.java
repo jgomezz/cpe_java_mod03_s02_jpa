094ead74bee345165ebf97d1539147de60877e66
jgomezz <jgomezz@gmail.com>
2026-05-03 10:59:41 -0500

    first version

---
+++ src/main/java/pe/edu/tecsup/app/CpeJavaMod03S02JpaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpeJavaMod03S02JpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpeJavaMod03S02JpaApplication.class, args);
+	}
+
+}
